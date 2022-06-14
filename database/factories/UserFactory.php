d7ebee1c8742fc23a85fc8ea27826fab15f3640b
Alexx <alexx1088@gmail.com>
2022-06-14 23:08:02 +0600

    seed and factory for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -12,17 +13,18 @@ class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
+
      * @return array<string, mixed>
      */
+
+    protected $model = User::class;
+
     public function definition()
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+
         ];
     }
 
